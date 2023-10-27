7cc4c2f8af5805805ee2f1f4b02e8dd97a85cba1
Biplob Das <biplob90.d@gmail.com>
2023-10-27 19:48:27 +0300

    modfied the database.js and deps.js

--- services/messageService.js
+++ services/messageService.js
@@ -1,13 +1,13 @@
-import db from "../database/database.js";
+import sql from "../database/database.js";
 
 
 const findAll = async () => {
-  const result = await db`SELECT * FROM messages order by id DESC LIMIT 5`;
+  const result = await sql`SELECT * FROM messages order by id DESC LIMIT 5`;
   return result;
 };
 
 const create = async (sender, message) => {
-  await db`INSERT INTO messages (sender, message)
+  await sql`INSERT INTO messages (sender, message)
     VALUES (${ sender }, ${ message })`;
 };
 export{findAll,create};
